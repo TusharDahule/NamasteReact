a49ee87340b64bf544b6c769ac28677ebe011ba8
TusharDahule <tushardahule@gmail.com>
2025-11-18 11:26:59 +0530

    [NEW] NPM scripts and React Components

--- App.js
+++ App.js
@@ -1,6 +1,27 @@
-import React from "react";
+// import React from "react";
 import ReactDOM from "react-dom/client";
 
-const h1 = React.createElement('h1', {}, 'Hello Tushar');
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(h1);
+// const h1 = React.createElement('h1', {}, 'Hello Tushar');
+const jsxHeading = (
+  <h1 id="heading" className="head">
+    Hello Tushar (From Heading React Element)
+  </h1>
+);
+
+// React Component & Component Composition
+const HeadingComponent = () => {
+    return (
+        <div>
+            <h2>From Heading Component</h2>
+            {jsxHeading}
+            <HeadingComponent2 />
+        </div>
+    )
+}
+
+const HeadingComponent2 = () => (
+    <h3>From Heading Component 2</h3>
+)
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent></HeadingComponent>);// This will take only React Element
